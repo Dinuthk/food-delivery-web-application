6dad71b83a5e992c94563a85ffd8d56e04d5a340
Dinuthk <141445703+Dinuthk@users.noreply.github.com>
2024-10-10 01:04:07 +0530

    fooditemConnectB

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -51,7 +51,7 @@ const List = ({url}) => {
             <img src={`${url}/images/`+item.image} alt="" />
             <p>{item.name}</p>
             <p>{item.category}</p>
-            <p>${item.price}</p>
+            <p>Rs: {item.price}</p>
             <p className='cursor'onClick={()=>removeFood(item._id)}>X</p>
           </div>
         )
